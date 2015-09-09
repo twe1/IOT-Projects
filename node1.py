5c964793135ae9a2884d74411cdd4dc024b22242
Twinkle-workPC <twinkzzmary@gmail.com>
2015-09-09 13:22:20 +0530

    2way comm

--- node1.py
+++ node1.py
@@ -2,21 +2,29 @@ import paho.mqtt.client as mqtt
 import time
 
 def on_connect(client,userdata,rc):
-	print "connected to broker " +str(rc)
-	client.subscribe("wa/kitchen")
+	client.subscribe("wa/kitchen2")
 
 def on_message(client, userdata, msg):
-	print "topic:",msg.topic," message:", str(msg.payload)
+	print "Topic:",msg.topic," Message from node2:", str(msg.payload)
+
+def on_publish(client,userdata, mid):
+	print "Data sent!"
 
 def on_disconnect(client,userdata, rc):
-	print "Disconnect:", +str(rc)
+	print "Disconnect:",
+	print rc
 	client.reconnect()
 
-client=mqtt.Client()
+client=mqtt.Client("wirewords2")
 client.on_connect=on_connect
 client.on_message=on_message
+client.on_publish=on_publish
 client.on_disconnect=on_disconnect
 client.connect("test.mosquitto.org",1883,60)
 
+print "Connected to broker.."
+
 while True:
+	mesg=raw_input("Enter the message: ")
+	client.publish("wa/kitchen", mesg,1)
 	client.loop()
--- node2.py
+++ node2.py
@@ -1,7 +1,14 @@
 import paho.mqtt.client as mqtt
 
+def on_connect(client,userdata,rc):
+	client.subscribe("wa/kitchen")
+
+def on_message(client, userdata, mesg):
+	print "Topic:",mesg.topic," Messagefrom node1:", str(mesg.payload)
+
 def on_publish(client,userdata, mid):
 	print "Data sent!"
+
 def on_disconnect(client,userdata, rc):
 	print "Disconnect: ",
 	print rc
@@ -10,11 +17,13 @@ def on_disconnect(client,userdata, rc):
 mqttc=mqtt.Client("wirewords")
 mqttc.on_publish=on_publish
 mqttc.on_disconnect=on_disconnect
-mqttc.connect("test.mosquitto.org",1883)
+mqttc.on_connect=on_connect
+mqttc.on_message=on_message
+mqttc.connect("test.mosquitto.org",1883,60)
 
 print "Connected to broker.."
 
 while True:
 	msg=raw_input("Enter the message: ")
-	mqttc.publish("wa/kitchen", msg,1)
+	mqttc.publish("wa/kitchen2", msg,1)
 	mqttc.loop()
