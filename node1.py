0f5a36646c4fad9851a3acd661058e44a5c7b9a2
Twinkle-workPC <twinkzzmary@gmail.com>
2015-09-09 11:52:40 +0530

    blocking loop forever removed

--- node1.py
+++ node1.py
@@ -1,16 +1,22 @@
 import paho.mqtt.client as mqtt
 import time
+
 def on_connect(client,userdata,rc):
-	print "Connected to broker.." +str(rc)
+	print "connected to broker " +str(rc)
 	client.subscribe("wa/kitchen")
 
 def on_message(client, userdata, msg):
 	print "topic:",msg.topic," message:", str(msg.payload)
+
 def on_disconnect(client,userdata, rc):
 	print "Disconnect:", +str(rc)
+	client.reconnect()
+
 client=mqtt.Client()
 client.on_connect=on_connect
 client.on_message=on_message
 client.on_disconnect=on_disconnect
 client.connect("test.mosquitto.org",1883,60)
-client.loop_forever()
+
+while True:
+	client.loop()
