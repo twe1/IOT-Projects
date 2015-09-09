2895bbc4289fe936ef6e4daabdfee546b695924f
Twinkle-workPC <twinkzzmary@gmail.com>
2015-09-09 11:21:17 +0530

    Code modified & getting output without loss
    after disconnect, the datas are resent but in diff order

--- node1.py
+++ node1.py
@@ -1,13 +1,16 @@
 import paho.mqtt.client as mqtt
+import time
 def on_connect(client,userdata,rc):
 	print "connected to broker " +str(rc)
 	client.subscribe("wa/kitchen")
 
 def on_message(client, userdata, msg):
 	print "topic:",msg.topic," message:", str(msg.payload)
-
+def on_disconnect(client,userdata, rc):
+	print "Disconnect:", +str(rc)
 client=mqtt.Client()
 client.on_connect=on_connect
 client.on_message=on_message
+client.on_disconnect=on_disconnect
 client.connect("test.mosquitto.org",1883,60)
 client.loop_forever()
--- node2.py
+++ node2.py
@@ -1,5 +1,20 @@
 import paho.mqtt.client as mqtt
+
+def on_publish(client,userdata, mid):
+	print "data sent"
+def on_disconnect(client,userdata, rc):
+	print "Disconnect: ",
+	print rc
+	mqttc.connect("test.mosquitto.org",1883)
+	
 mqttc=mqtt.Client("python_pub")
+mqttc.on_publish=on_publish
+mqttc.on_disconnect=on_disconnect
 mqttc.connect("test.mosquitto.org",1883)
-mqttc.publish("wa/kitchen", "wirewords academy")
-mqttc.loop(2)
+
+print "connected to broker"
+
+while True:
+	msg=raw_input("Enter the message: ")
+	mqttc.publish("wa/kitchen", msg,1)
+	mqttc.loop()
