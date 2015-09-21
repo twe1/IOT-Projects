cbebf1021b85d19e93ec4708d7d5267844b008cd
wirewords-pc <twinkzzmary@gmail.com>
2015-09-21 12:43:40 +0530

    handles database

---
+++ database.py
@@ -0,0 +1,18 @@
+class db():
+	def __init__(self,db_name="database.db"):
+		self.obj=sqlite3.connect(db_name)
+		self.cur=self.obj.cursor()
+
+	def create_table(self):
+		self.cur.execute('''CREATE TABLE tb(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, cmd STRING)''')
+
+	def insert(self,cmd):
+		time=datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
+		self.cur.execute('''INSERT INTO tb(time,cmd) VALUES(?,?)''',(time,cmd))
+		self.obj.commit() 
+
+	def fetch(self):
+		self.cur.execute('''SELECT time,cmd FROM tb ORDER BY time DESC LIMIT 1''',(time,cmd))
+		res=self.cur.fetchall()
+		return res[1]
+
