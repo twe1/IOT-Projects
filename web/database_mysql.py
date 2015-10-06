b8f26f6e6c0b7daa4854c110c1f4fb87532dbe40
wirewords-pc <twinkzzmary@gmail.com>
2015-10-06 10:52:26 +0530

    Minor change

--- web/database_mysql.py
+++ web/database_mysql.py
@@ -3,13 +3,18 @@ from datetime import datetime
 
 class database():
 	def __init__(self):
-		self.conn=pymysql.connect(host='127.0.0.1', user='root', passwd='',db='iot')
-		self.cur=self.conn.cursor()
+		self.obj=pymysql.connect(host='127.0.0.1', user='root', passwd='',db='iot')
+		self.cur=self.obj.cursor()
+		try:
+			self.cur.execute('''CREATE TABLE tb(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, status STRING)''')
+		except:
+			pass	
+
 		
 	def insert(self,status):
 		time=datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
 		self.cur.execute('''INSERT INTO tb(time,status) VALUES(%s,%s)''',(time,status))
-		self.conn.commit() 
+		self.obj.commit() 
 
 dbObj = database()
 
