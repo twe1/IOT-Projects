75a7acb9ab697e0f3424efa58c3d4a791faec648
wirewords-pc <twinkzzmary@gmail.com>
2015-10-06 10:51:46 +0530

    changing database engine from sqlite3 to mysql

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-import sqlite3
+import pymysql
 from datetime import datetime
 
 class db():
-	def __init__(self,db_name="rpi.db"):
-		self.obj=sqlite3.connect(db_name,check_same_thread=False)
+	def __init__(self,db_name='iot'):
+		self.obj=pymysql.connect(host='127.0.0.1', user='root', passwd='',db=db_name)
 		self.cur=self.obj.cursor()
 		try:
 			self.cur.execute('''CREATE TABLE tb(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, status STRING)''')
@@ -11,12 +11,10 @@ class db():
 			pass	
 
 
-	def create_table(self):
-		self.cur.execute('''CREATE TABLE tb(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, status STRING)''')
 
 	def insert(self,status):
 		time=datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
-		self.cur.execute('''INSERT INTO tb(time,status) VALUES(?,?)''',(time,status))
+		self.cur.execute('''INSERT INTO tb(time,status) VALUES(%s,%s)''',(time,status))
 		self.obj.commit() 
 
 	def fetch(self):
