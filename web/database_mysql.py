049d979555165e688689e8bd1cdf352a698c6d20
wirewords-pc <twinkzzmary@gmail.com>
2015-10-06 12:04:59 +0530

    No change

--- web/database_mysql.py
+++ web/database_mysql.py
@@ -5,11 +5,7 @@ class database():
 	def __init__(self):
 		self.obj=pymysql.connect(host='127.0.0.1', user='root', passwd='',db='iot')
 		self.cur=self.obj.cursor()
-		try:
-			self.cur.execute('''CREATE TABLE tb(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, status STRING)''')
-		except:
-			pass	
-
+		
 		
 	def insert(self,status):
 		time=datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
