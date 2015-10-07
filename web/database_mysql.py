d2f03390554e5ad388449b4920880622ec6a9d9f
wirewords-pc <twinkzzmary@gmail.com>
2015-10-07 11:26:32 +0530

    Bugs fixed

--- web/database_mysql.py
+++ web/database_mysql.py
@@ -2,25 +2,28 @@ import pymysql
 from datetime import datetime
 
 class database():
-	def __init__(self):
+	def insert(self,status):
 		self.obj=pymysql.connect(host='127.0.0.1', user='root', passwd='',db='sw')
 		self.cur=self.obj.cursor()
-		
-		
-	def insert(self,status):
+
 		time=datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
 		self.cur.execute('''INSERT INTO tb(time,status) VALUES(%s,%s)''',(time,status))
 		self.obj.commit() 
 
+		self.cur.close()
+
 	def fetch(self):
+		self.obj=pymysql.connect(host='127.0.0.1', user='root', passwd='',db='sw')
+		self.cur=self.obj.cursor()
+
 		self.cur.execute('''SELECT time,status FROM tb ORDER BY time DESC LIMIT 1''')
-		res=self.cur.fetchall()
+		res=self.cur.fetchone()
 		try:
-			res = res[0][1].encode('ascii')
+			res = res[1]
 		except Exception, e:
 			print e
 			
-		
+		self.cur.close()
 		return res
 
 dbObj = database()
