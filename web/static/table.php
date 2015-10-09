0af665390eeb5fcb659bd836d4beb32c5731084e
wirewords-pc <twinkzzmary@gmail.com>
2015-10-09 11:59:59 +0530

    testing on amazon

--- web/static/table.php
+++ web/static/table.php
@@ -1,8 +1,8 @@
 <?php 
     $hostname_connection = "localhost"; 
     $database_connection = "light"; 
-    $username_connection = "root"; 
-    $password_connection = ""; 
+    $username_connection = "admin"; 
+    $password_connection = "aaggss"; 
     $connection = mysql_connect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
     mysql_select_db($database_connection,$connection) or die( mysql_error("could not connect to database! " ) ) ; 
 ?>
